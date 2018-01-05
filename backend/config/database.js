ec5f793c51790e2616d532293dcd4d12be832e62
William Silva <contato.wasilva@hotmail.com>
2018-01-05 19:58:31 -0200

    Configuração Server e Databases

---
+++ backend/config/database.js
@@ -0,0 +1,4 @@
+const mongoose = require('mongoose')
+ module.exports = mongoose.connect('mongodb://localhost/wasinfo_db')
+//module.exports = mongoose.connect('mongodb://wasadmin:WASinfo1985@tatooine.mongodb.umbler.com:36748/wasinfo_bd')
+ 
