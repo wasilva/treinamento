1f4f96699f293695cca974c681157e0798e7aaba
William Silva <contato.wasilva@hotmail.com>
2018-01-05 22:32:02 -0200

    Rotas do Serviço de Ciclos de Pagamentos 2

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,7 +1,11 @@
 const express = require('express')
 
-module.exports = function(server){
+module.exports = function (server) {
     // API Routes
     const router = express.Router()
-    server.use('api', Router)    
+    server.use('/api', router)
+
+    // Rotas da API
+    const billingCycleService = require('../api/billingCycle/billingCycleService')
+    billingCycleService.register(router, '/billingCycles')
 }
