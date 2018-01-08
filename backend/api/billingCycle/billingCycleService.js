c762d425a47d5c7448457f800f1c939411e9ea3c
wasilva <contato.wasilva@hotmail.com>
2018-01-08 10:53:36 -0200

    Serviço count de ciclos de pagamento

--- backend/api/billingCycle/billingCycleService.js
+++ backend/api/billingCycle/billingCycleService.js
@@ -4,4 +4,15 @@ const BillingCycle = require('./billingCycle')
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({new : true, runValidators : true})
 
+BillingCycle.route('count', function(req, res, next) {
+// Serviço para contar os ciclos de pagamento gerado. Paginação do front-end.
+  BillingCycle.count(function(error, value){
+    if(error){
+      res.status(500).json({errors: [error]})
+    }else{
+      res.json({value})
+    }
+  })
+})
+
 module.exports = BillingCycle
