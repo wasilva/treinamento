41e3e69053ab33f94f1ab44cda4a247b7ee51a39
wasilva <contato.wasilva@hotmail.com>
2018-01-08 12:42:05 -0200

    Rota de Serviço Sumário de pagamento

--- backend/api/billingCycle/billingCycleService.js
+++ backend/api/billingCycle/billingCycleService.js
@@ -3,9 +3,9 @@ const BillingCycle = require('./billingCycle')
 // Gerenciamento da API REST
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({new : true, runValidators : true})
-
 BillingCycle.route('count', function(req, res, next) {
-// Serviço para contar os ciclos de pagamento gerado. Paginação do front-end.
+
+  // Serviço para contar os ciclos de pagamento gerado. Paginação do front-end.
   BillingCycle.count(function(error, value){
     if(error){
       res.status(500).json({errors: [error]})
