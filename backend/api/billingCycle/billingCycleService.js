141d81a00d315121eef7e4ca2de53fd7418c5070
William Silva <contato.wasilva@hotmail.com>
2018-01-08 00:29:30 -0200

    Validando GET, POST, DELETE, PUT das API's de pagamento

--- backend/api/billingCycle/billingCycleService.js
+++ backend/api/billingCycle/billingCycleService.js
@@ -1,6 +1,7 @@
 const BillingCycle = require('./billingCycle')
 
 // Gerenciamento da API REST
-BillingCycle.methods(['get', 'post', 'put' , 'delete'])
+BillingCycle.methods(['get', 'post', 'put', 'delete'])
+BillingCycle.updateOptions({new : true, runValidators : true})
 
 module.exports = BillingCycle
--- backend/config/database.js
+++ backend/config/database.js
@@ -2,3 +2,7 @@ const mongoose = require('mongoose')
  module.exports = mongoose.connect('mongodb://localhost/wasinfo_db')
 //module.exports = mongoose.connect('mongodb://wasadmin:WASinfo1985@tatooine.mongodb.umbler.com:36748/wasinfo_bd')
  
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
+mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite mínimo de {MIN}."
+mongoose.Error.messages.Number.max = "O '{VALUE}' informado é maior que o limite máximo de {MAX}."
+mongoose.Error.messages.String.enum = "'{VALUE}' não é válido para o atributo '{PATH}'."
