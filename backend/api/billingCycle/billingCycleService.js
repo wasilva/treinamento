2929d89020aeed2be9dd7c7aac99837a1bab4447
wasilva <contato.wasilva@hotmail.com>
2018-01-08 20:01:18 -0200

    Uniformizando mensagens de erro

--- backend/api/billingCycle/billingCycleService.js
+++ backend/api/billingCycle/billingCycleService.js
@@ -1,16 +1,40 @@
+const _ = require('lodash')
 const BillingCycle = require('./billingCycle')
 
 // Gerenciamento da API REST
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
-BillingCycle.updateOptions({new : true, runValidators : true})
-BillingCycle.route('count', function(req, res, next) {
+BillingCycle.updateOptions({ new: true, runValidators: true })
 
+// Tratativa das mensagens de erro. Retorna apenas o campo com erro
+BillingCycle.after('post', sendErrorsOrNext).after('put', sendErrorsOrNext)
+function sendErrorsOrNext(req, res, next)
+{
+  const bundle = res.locals.bundle
+
+  if (bundle.errors) {
+    var errors = parseErrors(bundle.errors)
+    res.status(500).json({ errors })
+  } else {
+    next()
+  }
+}
+
+function parseErrors(nodeRestfulErrors)
+{
+  const errors = []
+  _.forIn(nodeRestfulErrors, error => errors.push(error.message))
+  return errors
+}
+
+BillingCycle.route('count', function (req, res, next)
+{
   // Serviço para contar os ciclos de pagamento gerado. Paginação do front-end.
-  BillingCycle.count(function(error, value){
-    if(error){
-      res.status(500).json({errors: [error]})
-    }else{
-      res.json({value})
+  BillingCycle.count(function (error, value)
+  {
+    if (error) {
+      res.status(500).json({ errors: [error] })
+    } else {
+      res.json({ value })
     }
   })
 })
