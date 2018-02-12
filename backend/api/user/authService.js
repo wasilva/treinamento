369b8e72c86792eddf14a1df12399a318070fb2f
wasilva <contato.wasilva@hotmail.com>
2018-02-12 19:55:50 -0200

    Correção backend - Final

--- backend/api/user/authService.js
+++ backend/api/user/authService.js
@@ -23,6 +23,7 @@ const login = (req, res, next) => {
             return sendErrorsFromDB(res, err)
         } else if (user && bcrypt.compareSync(password, user.password)) {
             const token = jwt.sign(user, env.authSecret, {
+                // expiresIn: "1 day"
                 expiresIn: "1 day"
             })
             const { name, email } = user
