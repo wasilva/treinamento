2fcce74274bc2ea6c961a8b5ac8947f4b742bab5
wasilva <contato.wasilva@hotmail.com>
2018-03-02 20:04:07 -0300

    Config database

--- config/database.js
+++ config/database.js
@@ -4,7 +4,8 @@ mongoose.Promise = require("bluebird")
 // module.exports = mongoose.createConnection('mongodb://localhost/db_finance', { useMongoClient: true })
 // module.exports = mongoose.connect('mongodb://localhost/db_finance')
 // module.exports = mongoose.connect('mongodb://localhost/wasinfo_db', { useMongoClient: true })
-module.exports = mongoose.connect('mongodb://wasinfo_db:WASinfo1985@mongo_wasinfo_db:27017/wasinfo_db', { useMongoClient: true })
+module.exports = mongoose.connect('mongodb://wasinfo_db:WASinfo1985@mongo_wasinfo_db:27017/wasinfo_db', { useMongoClient: true }) // Externo
+// module.exports = mongoose.connect('mongodb://wasinfo_db:WASinfo1985@naboo.mongodb.umbler.com:49480/wasinfo_db', { useMongoClient: true }) // Interno
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
 mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
